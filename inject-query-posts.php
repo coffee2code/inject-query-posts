1d53d7a391bd1b714b274c4cbeae25934468a56d
Scott Reilly <scott@coffee2code.com>
2010-09-27 12:31:07 -0400

    Release v2.0:
    * If no query_obj is sent, use the global `$wp_query object` (previously used to create a new query object)
    * If a not-false, non-object value is sent as $wp_query object (namely any non-empty string or non-zero integer), then create a new WP_Query object for use
    * Add ability to preserve the state of the existing query_obj
        * Add `$preserve_query_obj` arg (optional) to `inject_query_posts()`, default to true
        * Add filter 'inject_query_posts_preserve_query_obj' that gets passed value of `$preserve_query_obj`
    * Reset more query_obj settings
    * Wrap function in `if(!function_exists())` check
    * Remove docs from top of plugin file (all that and more are in readme.txt)
    * Minor code reformatting (spacing)
    * Add PHPDoc documentation
    * Note compatibility with WP 2.8+, 2.9+, 3.0+
    * Update copyright date
    * Add package info to top of plugin file
    * Add Changelog, Filters, Template Tags, and Upgrade Notice sections to readme.txt
    * Remove trailing whitespace

--- inject-query-posts.php
+++ inject-query-posts.php
@@ -1,64 +1,34 @@
 <?php
+/**
+ * @package Inject_Query_Posts
+ * @author Scott Reilly
+ * @version 2.0
+ */
 /*
 Plugin Name: Inject Query Posts
-Version: 1.0
-Plugin URI: http://coffee2code.com/wp-plugins/inject-query-posts
+Version: 2.0
+Plugin URI: http://coffee2code.com/wp-plugins/inject-query-posts/
 Author: Scott Reilly
 Author URI: http://coffee2code.com
 Description: Inject an array of posts into a WP query object as if queried, particularly useful to allow use of standard template tags.
 
-WordPress's template tags are intended to be used within 'the loop'.  The loop is managed by a WP_Query object which sets up various
-global variables and its own object variables for use by the various template tags.  The primary purpose of a WP_Query object is to
-actually query the database for the posts that match the currently specified criteria.  However, if you don't need to query for posts
-since you already have them by some other means, you can still take advantage of the template tags by injecting those posts into the
-WP_Query via this plugin.
-
-Depending on the template tags you are looking to use, or the logic you are hoping to employ within a loop, you may need to manually
-configure some of the query object's variables.
-
-Example:
-
-<?php
-// Say we're in the sidebar
-
-// We've gotten some post object on our own.
-$posts = c2c_get_random_posts(5, '');
-// Inject the posts
-inject_query_posts($posts);
-// Now let's display them via template tags:
-if (have_posts()) :
-    while (have_posts()) : the_post(); ?>
-
-        <li><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></li>
-
-    <?php endwhile;?>
-<?php endif; ?>
-
-Compatible with WordPress 2.3+, 2.5+, 2.6+, 2.7+
+Compatible with WordPress 2.3+, 2.5+, 2.6+, 2.7+, 2.8+, 2.9+, 3.0+
 
 NOTE: Injecting posts into a query object will cause that object to forget about previous posts it may have retrieved.  You probably
 only want to do this outside of any existing loops, and create your own custom loop after the injection.
 
-=>> Read the accompanying readme.txt file for more information.  Also, visit the plugin's homepage
-=>> for more information and the latest updates
-
-Installation:
-
-1. Download the file http://coffee2code.com/wp-plugins/inject-query-posts.zip and unzip it into your 
-/wp-content/plugins/ directory.
-2. Activate the plugin through the 'Plugins' admin menu in WordPress
-3. Use the inject_query_posts() function to inject an array of posts into a WP query object.  Specify the posts array as the
-first argument.  Configure the query object by passing an array as the second argument.  If specifying a WP query object, pass
-it as the third object; if not specified then the global wp_query object will be assumed.
+=>> Read the accompanying readme.txt file for instructions and documentation.
+=>> Also, visit the plugin's homepage for additional information and updates.
+=>> Or visit: http://wordpress.org/extend/plugins/inject_query_posts/
 
 */
 
 /*
-Copyright (c) 2008-2009 by Scott Reilly (aka coffee2code)
+Copyright (c) 2008-2010 by Scott Reilly (aka coffee2code)
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
-files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
-modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the 
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
+modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
 Software is furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
@@ -67,36 +37,81 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
 LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
 IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
 */
 
-function inject_query_posts( $posts, $config = array(), $query_obj = null ) {
+if ( !function_exists( 'inject_query_posts' ) ) :
+/**
+ * Injects an array of posts into a query object as if that query object had
+ * obtained those posts via a query.
+ *
+ * NOTE: If you wish to fully simulate a query despite the fact that the query
+ * object is not performing the actual database query, you must specify the
+ * a query via $config (i.e. array('query'=>'cat=5')) and/or directly set
+ * query object variables via $config.
+ *
+ * NOTE: By default, the function assumes the posts being injected will
+ * invalidate any existing state in the query object, so that query object will
+ * be reset prior to post inject.  Set $preserve_query_obj to true to retain
+ * the state of the query object.
+ *
+ * NOTE: In order for 'query' to be configured via $config, $preserve_query_obj
+ * must be false.
+ *
+ * @param array $posts Array of posts to inject into the query object.
+ * @param array $config (optional) Associative array of query object variables to directly set, and their values.
+ * @param WP_Query|null $query_obj (optional) The query object to modify. If null, then the global wp_query object will be used. Pass a string or non-zero integer to have a new query object created and used.
+ * @param bool $preserve_query_obj (optional) Should the query object be re-initialized (i.e. reset) prior to injecting posts?
+ * @return array The originally passed in array of posts.
+ */
+function inject_query_posts( $posts, $config = array(), $query_obj = null, $preserve_query_obj = true ) {
 	$posts = (array) $posts;
+	$preserve_query_obj = apply_filters( 'inject_query_posts_preserve_query_obj', $preserve_query_obj, $query_obj );
 	if ( !$query_obj ) {
 		global $wp_query;
-		$query_obj = new WP_Query();
+		$query_obj = $wp_query;
 	}
+	if ( !is_object( $query_obj ) )
+		$query_obj = new WP_Query();
+
 	// Initialize the query object
-	if ( isset($config['query']) )
-		$query_obj->parse_query($config['query']); // This calls init() itself, so no need to do it here
-	else
-		$query_obj->init();
-	foreach ( $config as $key => $value ) {
-		if ( 'query' == $key ) continue;
+	if ( !$preserve_query_obj ) {
+		// As of WP 2.9.1, these object variables are not resettable except directly
+		$query_obj->post = '';
+		$query_obj->request = '';
+		$query_obj->found_posts = 0;
+		$query_obj->max_num_pages = 0;
+		$query_obj->comments = '';
+		$query_obj->comment_count = 0;
+		$query_obj->current_comment = -1;
+		$query_obj->comment = '';
+		$query_obj->max_num_pages = 0;
+		$query_obj->max_num_comment_pages = 0;
+		$query_obj->is_preview = false;
+		$query_obj->is_comments_popup = false;
+
+		if ( isset( $config['query'] ) )
+			$query_obj->parse_query( $config['query'] ); // This calls init() itself, so no need to do it here
+		else
+			$query_obj->init();
+	}
+	foreach ( (array) $config as $key => $value ) {
+		if ( 'query' == $key )
+			continue;
 		$query_obj->$key = $value;
 	}
 	// Load the posts into the query object
 	$query_obj->posts = $posts;
-	update_post_caches($posts);
-	$query_obj->post_count = count($posts);
+	update_post_caches( $posts );
+	$query_obj->post_count = count( $posts );
 	if ( $query_obj->post_count > 0 ) {
 		$query_obj->post = $posts[0];
 		$query_obj->found_posts = $query_obj->post_count;
 	}
-	if ( !isset($config['is_404']) ) // Unless explicitly told to be a 404, don't be a 404
+	if ( !isset( $config['is_404'] ) ) // Unless explicitly told to be a 404, don't be a 404
 		$query_obj->is_404 = false;
-	$wp_query = $query_obj; // This only has any effect if wp_query was previously declared as global
+	$wp_query = $query_obj; // This only has effect if $wp_query was previously declared global
 	return $posts;
 }
+endif;
 
 ?>
--- readme.txt
+++ readme.txt
@@ -1,11 +1,11 @@
 === Inject Query Posts ===
-Contributors: Scott Reilly
-Donate link: http://coffee2code.com
-Tags: wp_query, query, posts, loop, template tags
+Contributors: coffee2code
+Donate link: http://coffee2code.com/donate
+Tags: wp_query, query, posts, loop, template tags, coffee2code
 Requires at least: 2.3
-Tested up to: 2.7.1
-Stable tag: trunk
-Version: 1.0
+Tested up to: 3.0.1
+Stable tag: 2.0
+Version: 2.0
 
 Inject an array of posts into a WP query object as if queried, particularly useful to allow use of standard template tags.
 
@@ -19,6 +19,7 @@ Depending on the template tags you are looking to use, or the logic you are hopi
 
 Example:
 
+`
 <?php
 // Say we're in the sidebar
 
@@ -34,11 +35,99 @@ if (have_posts()) :
 
     <?php endwhile;?>
 <?php endif; ?>
-
+`
 
 == Installation ==
 
-1. Unzip `inject-query-posts-v1.0.zip` inside the `/wp-content/plugins/` directory for your site
+1. Unzip `inject-query-posts.zip` inside the `/wp-content/plugins/` directory for your site (or install via the built-in WordPress plugin installer)
 1. Activate the plugin through the 'Plugins' admin menu in WordPress
 1. Use the `inject_query_posts()` function to inject an array of posts into a WP query object.  Specify the posts array as the first argument.  Configure the query object by passing an array as the second argument.  If specifying a WP query object, pass it as the third object; if not specified then the global wp_query object will be assumed.
 
+
+== Template Tags ==
+
+The plugin provides one template tag for use in your theme templates, functions.php, or plugins.
+
+= Functions =
+
+* `<?php function inject_query_posts( $posts, $config = array(), $query_obj = null, $preserve_query_obj = true ) ?>`
+Injects an array of posts into a query object as if that query object had obtained those posts via a query.
+
+= Arguments =
+
+* `$posts` (array)
+Array of posts to inject into the query object.
+
+* `$config` (array)
+Optional.  Associative array of query object variables to directly set, and their values.
+
+* `$query_obj` (WP_Query|null)
+Optional.  The query object to modify. If null, then the global wp_query object will be used. Pass a string or non-zero integer to have a new query object created and used.
+
+* `$preserve_query_obj` (bool)
+Optional.  Should the query object be re-initialized (i.e. reset) prior to injecting posts?
+
+= Examples =
+
+* (See Description section for an additional example.)
+
+* Similar to previous example, for WP 3.0+
+
+`
+<?php
+$posts = c2c_get_random_posts( 5, '' ); // Obtain posts via your favorite related posts, find posts, etc plugin, or via custom query
+inject_query_posts( $posts );
+get_template_part('loop');
+?>
+`
+
+== Filters ==
+
+The plugin is further customizable via one filter. Typically, these customizations would be put into your active theme's functions.php file, or used by another plugin.
+
+= inject_query_posts_preserve_query_obj (filter) =
+
+The 'inject_query_posts_preserve_query_obj' filter allows you override the value of the `$preserve_query_obj` argument passed to the function.  This is not typical usage for most users.
+
+Arguments:
+
+* $preserve_query_obj (bool) : Boolean indicating if the query object was set to be preserved or not
+* $query_obj (WP_Query object) : The WP_Query object passed to the `inject_query_posts()`
+
+Example:
+
+`// Never preserve the condition of the WP_Query object
+add_filter( 'inject_query_posts_preserve_query_obj', 'my_preserve_query_obj', 10, 2 );
+function my_preserve_query_obj( $preserve_query_obj, $query_obj ) {
+	return false;
+}`
+
+
+== Changelog ==
+
+= 2.0 =
+* If no query_obj is sent, use the global $wp_query object (previously used to create a new query object)
+* If a not-false, non-object value is sent as $wp_query object (namely any non-empty string or non-zero integer), then create a new WP_Query object for use
+* Add ability to preserve the state of the existing query_obj
+    * Add $preserve_query_obj arg (optional) to inject_query_posts(), default to true
+    * Add filter 'inject_query_posts_preserve_query_obj' that gets passed value of $preserve_query_obj
+* Reset more query_obj settings
+* Wrap function in if(!function_exists()) check
+* Remove docs from top of plugin file (all that and more are in readme.txt)
+* Minor code reformatting (spacing)
+* Add PHPDoc documentation
+* Note compatibility with WP 2.8+, 2.9+, 3.0+
+* Update copyright date
+* Add package info to top of plugin file
+* Add Changelog, Filters, Template Tags, and Upgrade Notice sections to readme.txt
+* Remove trailing whitespace
+* Add to plugin repo
+
+= 1.0 =
+* Initial release
+
+
+== Upgrade Notice ==
+
+= 2.0 =
+Recommended major update! Highlights: now use global WP_Query object by default; added ability to preserve state of existing query object; misc non-functionality changes; expanded readme.txt text; verified WP 3.0 compatibility.
