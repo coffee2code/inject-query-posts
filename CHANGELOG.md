9e1b33a5ae1827f532aa314e076d45e9fa12aaa9
Scott Reilly <scott@coffee2code.com>
2019-02-12 23:20:47 -0700

    Update License URI to be HTTPS

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,7 @@
 
 ## _(in-progress)_
 * New: Add CHANGELOG.md and move all but most recent changelog entries into it
+* Change: Update License URI to be HTTPS
 
 ## 2.2.6 _(2018-06-12)_
 * New: Add README.md
--- inject-query-posts.php
+++ inject-query-posts.php
@@ -7,7 +7,7 @@
  * Author URI:  http://coffee2code.com/
  * Text Domain: inject-query-posts
  * License:     GPLv2 or later
- * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Description: Facilitates injecting an array of posts into a WP query object as if queried. Particularly useful to allow use of standard template tags.
  *
  * Compatible with WordPress 3.6 through 4.9+.
