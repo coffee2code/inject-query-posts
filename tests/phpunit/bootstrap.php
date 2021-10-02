01d7aa905050822f287f7580d95be11086b1a060
Scott Reilly <scott@coffee2code.com>
2021-10-01 23:35:44 -0700

    Unit tests: Restructure unit test directories
    * Move `phpunit/` into `tests/phpunit/`
    * Move `phpunit/bin/` into `tests/`

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -20,7 +20,7 @@ require_once $_tests_dir . '/tests/phpunit/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/inject-query-posts.php';
+	require dirname( __FILE__, 3 ) . '/inject-query-posts.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
